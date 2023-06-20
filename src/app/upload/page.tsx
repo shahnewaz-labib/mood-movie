39b816174c8a50c5f693947907804284259d1835
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2023-06-21 04:06:03 +0600

    upload button added

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,8 +1,13 @@
 export default function uploadPage() {
   return (
-    <input
-      type="file"
-      className="file-input-bordered file-input-success file-input w-full max-w-xs"
-    />
+    <div className="flex h-screen items-center justify-center">
+      <div className="m-auto">
+        <h1 className="p-10 text-4xl">Upload an image</h1>
+        <input
+          type="file"
+          className="file-input-bordered file-input-success file-input w-full max-w-xs"
+        />
+      </div>
+    </div>
   );
 }
