798bc6901bd170f103d704a67b7dd7bdf89bf293
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2023-06-21 23:52:01 +0600

    some css fixes

--- src/app/details/[movieid]/page.tsx
+++ src/app/details/[movieid]/page.tsx
@@ -12,47 +12,49 @@ export default async function page({
   console.log(movie);
 
   return (
-    <div className="flex justify-center pt-12">
-      <div className="max-w-[1400px]">
-        <div className="flex">
-          <div className="flex-1">
-            <Image
-              className="h-auto w-full"
-              src={basePosterUrl + movie.poster_path}
-              alt={movie.title}
-              width={600}
-              height={900}
-            />
-          </div>
-          <div className="ml-12 flex-1 text-white">
-            <h1 className="text-5xl font-bold">{movie.title}</h1>
-            <br /> {/* Add an empty line here */}
-            <p className="text-2xl">
-              <strong className="text-3xl">
-                {new Date(movie.release_date) > new Date()
-                  ? "Unreleased"
-                  : "Released"}
-                :
-              </strong>{" "}
-              {new Date(movie.release_date).getFullYear()}
-            </p>
-            <p className="text-2xl">
-              <strong className="text-3xl">Rating:</strong> ⭐{" "}
-              {movie.vote_average.toString().slice(0, 3)}
-            </p>
-            <p className="text-2xl">
-              <strong className="text-3xl">Genre:</strong>{" "}
-              {movie.genres.map((genre) => genre.name).join(", ")}
-            </p>
-            <p className="text-2xl">
-              <strong className="text-3xl">Runtime:</strong> {movie.runtime}{" "}
-              minutes
-            </p>
-            <br /> {/* Add an empty line here */}
-            <p className="text-2xl">
-              <strong className="text-3xl">Plot:</strong> <br />{" "}
-              {movie.overview}
-            </p>
+    <div className="relative bg-cover bg-fixed bg-center bg-no-repeat">
+      <div className="flex justify-center pt-12">
+        <div className="max-w-[1000px]">
+          <div className="flex">
+            <div className="flex-1">
+              <Image
+                className="h-auto w-full"
+                src={basePosterUrl + movie.poster_path}
+                alt={movie.title}
+                width={600}
+                height={900}
+              />
+            </div>
+            <div className="ml-12 flex-1 text-white">
+              <h1 className="text-5xl font-bold">{movie.title}</h1>
+              <br /> {/* Add an empty line here */}
+              <p className="text-2xl">
+                <strong className="text-2xl">
+                  {new Date(movie.release_date) > new Date()
+                    ? "Unreleased"
+                    : "Released"}
+                  :
+                </strong>{" "}
+                {new Date(movie.release_date).getFullYear()}
+              </p>
+              <p className="text-2xl">
+                <strong className="text-2xl">Rating:</strong> ⭐{" "}
+                {movie.vote_average.toString().slice(0, 3)}
+              </p>
+              <p className="text-2xl">
+                <strong className="text-2xl">Genre:</strong>{" "}
+                {movie.genres.map((genre) => genre.name).join(", ")}
+              </p>
+              <p className="text-2xl">
+                <strong className="text-2xl">Runtime:</strong> {movie.runtime}{" "}
+                minutes
+              </p>
+              <br /> {/* Add an empty line here */}
+              <p className="text-2xl">
+                <strong className="text-2xl">Plot:</strong> <br />{" "}
+                {movie.overview}
+              </p>
+            </div>
           </div>
         </div>
       </div>
